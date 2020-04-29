4f4478ab85c2775122df18bebe10fa6d09b13676
aliyasser20 <aliyasser20@hotmail.com>
2020-04-29 19:30:08 -0400

    dashboard stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @productsCount = Product.count
+    @categoriesCount = Category.count
   end
 end
